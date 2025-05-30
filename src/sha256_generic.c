99cb16a60b350fc10e5fe06b11e772ec434a8dbb
Potuz <potuz@prysmaticlabs.com>
2025-05-30 19:39:02 -0300

    Don't use bswap

--- src/sha256_generic.c
+++ src/sha256_generic.c
@@ -27,9 +27,7 @@ Such software is licensed under the BSD 3-Clause License and is
 Copyright (c) 2012-2023, Intel Corporation
 */
 
-#include <byteswap.h>
 #include <stdint.h>
-#include <string.h>
 
 static const uint32_t init[] = {
     0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
@@ -120,7 +118,14 @@ void hashtree_sha256_generic(unsigned char* output, const unsigned char* input, 
         h += init[7];
 
         // Rounds with padding
-        uint32_t digest[8] = {a, b, c, d, e, f, g, h};
+        uint32_t h0 = a;
+        uint32_t h1 = b;
+        uint32_t h2 = c;
+        uint32_t h3 = d;
+        uint32_t h4 = e;
+        uint32_t h5 = f;
+        uint32_t h6 = g;
+        uint32_t h7 = h;
         for (int i = 0; i < 64; i++) {
             uint32_t t1 = h + (rotr(e, 6) ^ rotr(e, 11) ^ rotr(e, 25)) + ((e & f) ^ (~e & g)) + P[i];
             uint32_t t2 = (rotr(a, 2) ^ rotr(a, 13) ^ rotr(a, 22)) + ((a & b) ^ (a & c) ^ (b & c));
@@ -134,17 +139,45 @@ void hashtree_sha256_generic(unsigned char* output, const unsigned char* input, 
             a = t1 + t2;
         }
 
-        digest[0] += a;
-        digest[1] += b;
-        digest[2] += c;
-        digest[3] += d;
-        digest[4] += e;
-        digest[5] += f;
-        digest[6] += g;
-        digest[7] += h;
-        for (int i = 0; i < 8; i++) {
-            uint32_t val = __builtin_bswap32(digest[i]);
-            memcpy(&output[k * 32 + (i << 2)], &val, 4);
-        }
+        h0 += a;
+        h1 += b;
+        h2 += c;
+        h3 += d;
+        h4 += e;
+        h5 += f;
+        h6 += g;
+        h7 += h;
+        output[k * 32 + 0] = (h0 >> 24) & 0xFF;
+        output[k * 32 + 1] = (h0 >> 16) & 0xFF;
+        output[k * 32 + 2] = (h0 >> 8) & 0xFF;
+        output[k * 32 + 3] = h0 & 0xFF;
+        output[k * 32 + 4] = (h1 >> 24) & 0xFF;
+        output[k * 32 + 5] = (h1 >> 16) & 0xFF;
+        output[k * 32 + 6] = (h1 >> 8) & 0xFF;
+        output[k * 32 + 7] = h1 & 0xFF;
+        output[k * 32 + 8] = (h2 >> 24) & 0xFF;
+        output[k * 32 + 9] = (h2 >> 16) & 0xFF;
+        output[k * 32 + 10] = (h2 >> 8) & 0xFF;
+        output[k * 32 + 11] = h2 & 0xFF;
+        output[k * 32 + 12] = (h3 >> 24) & 0xFF;
+        output[k * 32 + 13] = (h3 >> 16) & 0xFF;
+        output[k * 32 + 14] = (h3 >> 8) & 0xFF;
+        output[k * 32 + 15] = h3 & 0xFF;
+        output[k * 32 + 16] = (h4 >> 24) & 0xFF;
+        output[k * 32 + 17] = (h4 >> 16) & 0xFF;
+        output[k * 32 + 18] = (h4 >> 8) & 0xFF;
+        output[k * 32 + 19] = h4 & 0xFF;
+        output[k * 32 + 20] = (h5 >> 24) & 0xFF;
+        output[k * 32 + 21] = (h5 >> 16) & 0xFF;
+        output[k * 32 + 22] = (h5 >> 8) & 0xFF;
+        output[k * 32 + 23] = h5 & 0xFF;
+        output[k * 32 + 24] = (h6 >> 24) & 0xFF;
+        output[k * 32 + 25] = (h6 >> 16) & 0xFF;
+        output[k * 32 + 26] = (h6 >> 8) & 0xFF;
+        output[k * 32 + 27] = h6 & 0xFF;
+        output[k * 32 + 28] = (h7 >> 24) & 0xFF;
+        output[k * 32 + 29] = (h7 >> 16) & 0xFF;
+        output[k * 32 + 30] = (h7 >> 8) & 0xFF;
+        output[k * 32 + 31] = h7 & 0xFF;
     }
 }
